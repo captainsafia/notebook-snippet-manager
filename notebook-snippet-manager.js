05a9e617b003c2456d3df31a0ac6bd42259a46a3
Safia Abdalla <safia@safia.rocks>
2016-03-29 13:41:52 -0500

    Added initial table of snippets

--- notebook-snippet-manager.js
+++ notebook-snippet-manager.js
@@ -55,10 +55,38 @@ define(['base/js/namespace',
         });
     }
 
+    function get_snippets() {
+        var storage = window['localStorage'];
+        var stored_snippets = storage.getItem('JupyterNotebookSnippets');
+        if (stored_snippets == null) {
+            return {};
+        } else {
+            return JSON.parse(stored_snippets);
+        }
+    }
+
     function add_cell_from_snippet_manager() {
         var selected_index = Jupyter.notebook.get_selected_index();
 
         var modal_content = $('<p/>').html('Select a snippet to insert.');
+        modal_content.append('<br><br>');
+        var list_content = $('<div id="snippets" style="white-space: pre-wrap;">');
+        list_content.append('<input class="search" placeholder="Search Snippets">');
+        list_content.append('<br><br>');
+
+        var table = $('<table class="table"><tbody class="list"></tbody></table>');
+        var table_children = table.children();
+        table_children.append('<tr><th>Snippet Name</th><th>Snippet Content</th></tr>');
+
+        var snippets = get_snippets();
+        for (var index in snippets) {
+            var snippet = snippets[index];
+            table_children.append('<tr><td class="name">' + index + '</td>' +
+                                    '<td class="content">' + snippet + '</td></tr>');
+        }
+
+        list_content.append(table);
+        modal_content.append(list_content);
 
         dialog.modal({
             title: 'Select A Snippet to Include',
@@ -114,7 +142,6 @@ define(['base/js/namespace',
                 'buttons': {OK: {}}
             });
         }
-        console.log(list);
     }
 
     return {
